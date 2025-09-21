bab6d79261e7488b47bb1be95fabe3513adc4e65
Armi-nn <151467800+Armi-nn@users.noreply.github.com>
2025-09-21 22:00:22 +0330

    Add PageNotFound

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,9 +1,95 @@
-export const metadata={
-    title:'',
-    description:''
-}
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import image from "@/assets/404.png";
+import Link from "next/link";
+import Navbar from "@/Components/Navbar/page";
+import Footer from "@/Components/Footer/page";
+
+export const metadata = {
+  title: "NotFound",
+  description: "PageNotFound(404)",
+};
 export default function notFond() {
   return (
-    <div>notFond</div>
-  )
+    <>
+      <Navbar />
+      <Box>
+        <Box
+          sx={{
+            mt: "10px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "700px",
+              height: "420px",
+              position: "relative",
+            }}
+          >
+            <Image fill src={image} alt="404" />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#EFA71A",
+              fontSize: "37px",
+              fontWeight: "bold !important",
+              mt: "25px",
+            }}
+          >
+            ! صفحه مورد نظر شما پیدا نشد
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Link href={"/"}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                border: "1px solid #EFA71A",
+                width: "200px",
+                height: "55px",
+                borderRadius: "50px",
+                transition: "all 0.5s ease",
+                "&:hover": {
+                  backgroundColor: "#EFA71A",
+                },
+                "&:hover p": {
+                  color: "#fff",
+                  backgroundColor: "#EFA71A",
+                },
+                cursor: "pointer",
+                mt: "25px",
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "#EFA71A",
+                  fontSize: "18px",
+                  transition: "all 0.5s ease",
+                }}
+              >
+                رفتن به صفحه اصلی
+              </Typography>
+            </Box>
+          </Link>
+        </Box>
+      </Box>
+      <Footer />
+    </>
+  );
 }
